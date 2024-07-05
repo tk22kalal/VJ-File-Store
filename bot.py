97358ced2b9e072ffa7f4148271eb499aa4e250c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-05 15:33:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ async def start():
     await app.setup()
     bind_address = "0.0.0.0"
     await web.TCPSite(app, bind_address, Var.PORT).start()
-    await restart_bots()
+    
     print("Bot Started Powered By @VJ_Botz")
     await idle()
 
